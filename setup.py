105d788b6ce05cf83d15c91b9282132ca9510d9e
Chahyon Ku <chahyon@umich.edu>
2026-03-04 11:44:24 -0500

    split up forward

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="lang-seg",
-    packages=find_packages(include=["lang_seg"])
+    packages=find_packages(include=["lang_seg"]),
+    install_requires=[
+        "pytorch-lightning",
+        "openai-clip",
+        "torch-encoding @ git+https://github.com/zhanghang1989/PyTorch-Encoding",
+        "pandas"
+    ]
 )
